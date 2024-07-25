84a9b279fcac39b3395bd4b5ee630f009a49f721
Michael Osborne <mjosborne1@gmail.com>
2024-07-26 07:43:13 +1000

    Change url for value set

--- builder.py
+++ builder.py
@@ -35,19 +35,19 @@ def get_preferred_term(code):
 
 def write_header(outfile):
     with open(outfile, 'w') as file:
-        file.write('ValueSet: RANZCRRadiologyReferral\n')
-        file.write('Id: ranzcr-radiology-referral\n')
+        file.write('ValueSet: RANZCRRadiologyProcedures\n')
+        file.write('Id: ranzcr-radiology-procedures\n')
         file.write('Title: "RANZCR Referral Procedures"\n')
         file.write('Description: "Standard codes for use in requesting radiology tests in Australia, derived from the RANZCR Radiology Referral Set (RRS)."\n')
         file.write('* ^meta.profile[+] = "http://hl7.org/fhir/StructureDefinition/shareablevalueset"\n')
-        file.write('* ^url = "https://ranzcr.com/fhir/ValueSet/radiology-referral"\n')
+        file.write('* ^url = "https://ranzcr.com/fhir/ValueSet/radiology-procedures"\n')
         file.write('* ^version = "1.0.0"\n')
         file.write('* ^extension[http://hl7.org/fhir/StructureDefinition/structuredefinition-fmm].valueInteger = 0\n')
         file.write('* ^status = #draft\n')
         file.write('* ^experimental = false\n')
         file.write('* ^date = "2024-07-17"\n')
         file.write('* ^publisher = "HL7 Australia"\n')
-
+        file.write('* ^copyright = "Copyright \u00a9 2023 Royal Australian and New Zealand College of Radiologists- All rights reserved. This resource includes SNOMED Clinical Terms\u2122 (SNOMED CT\u00ae) which is used by permission of the International Health Terminology Standards Development Organisation (IHTSDO). All rights reserved. SNOMED CT\u00ae, was originally created by The College of American Pathologists. \u201cSNOMED\u201d and \u201cSNOMED CT\u201d are registered trademarks of the IHTSDO. \n\nThe rights to use and implement or implementation of SNOMED CT content are limited to the extent it is necessary to allow for the end use of this material.  No further rights are granted in respect of the International Release and no further use of any SNOMED CT content by any other party is permitted."')
 
 def build_rrv_fshfile(infile,outdir):
     fsh_lines = []
